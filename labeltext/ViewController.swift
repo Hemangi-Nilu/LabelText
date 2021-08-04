0b4524c07bb462b259983dfd813ddd6a0dccf6ac
Dhara <dharasarvaiya.nilu@gmail.com>
2021-08-04 11:02:41 +0530

    Initial Commit

---
+++ labeltext/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  labeltext
+//
+//  Created by Mac Mini on 04/08/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
